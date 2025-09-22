bfb549e9707cb9e4dde2587059470b36ef6cb291
nohren <kuritake@protonmail.com>
2025-09-22 16:38:34 -0700

    finished debugging ttgo mcu

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,13 @@
 #include <Arduino.h>
 #include <TFT_eSPI.h>
 #include "config.h"
-// #include "MoistureSensor.h"
-// #ifdef ESP32
-// #include <esp_sleep.h>
-// #include "Pwm.h"
-// #endif
+#include "MoistureSensor.h"
+#ifdef ESP32
+#include <esp_sleep.h>
+#include "Pwm.h"
+#endif
 
-// SharedState state;
+SharedState state;
 TFT_eSPI tft;
 
 // internal setup
@@ -30,58 +30,56 @@ void setup()
     tft.setRotation(1);
     tft.fillScreen(TFT_BLACK);
     tft.setTextColor(TFT_WHITE, TFT_BLACK);
-    tft.drawString("Mode: INIT", 20, 20, 2);
-    // // Init state
-    // setState(state, Action::INIT_SETUP);
+    // Init state
+    setState(state, Action::INIT_SETUP);
 
-    // Init sensor if in monitor mode
-    // if (state.mode == Mode::MONITOR)
-    // {
-    //     initSensor(PIN_SENSOR_PWR, PIN_SOIL_ADC);
-    // }
+    //Init sensor if in monitor mode
+    if (state.mode == Mode::MONITOR)
+    {
+        initSensor(PIN_SENSOR_PWR, PIN_SOIL_ADC);
+    }
 
     // INIT PWM
 #ifdef ESP32
 // set ADC resolution and attenuation
-    //initPWM(PIN_PUMP_GATE, PWM_CH, PWM_FREQ, PWM_RES_BITS);
+    initPWM(PIN_PUMP_GATE, PWM_CH, PWM_FREQ, PWM_RES_BITS);
 #endif
     //print configuration and flash for 10 seconds to show ready
     // no delays after setup, only use timing to avoid blocking and important tasks like button handling
     //printConfiguration(state);
-    // for (int i = 0; i < 5; i++) {
-    //     digitalWrite(LED_BUILTIN, HIGH);
-    //     digitalWrite(PIN_LED_LARGE, HIGH);
-    //     delay(1000);
-    //     digitalWrite(LED_BUILTIN, LOW);
-    //     digitalWrite(PIN_LED_LARGE, LOW);
-    //     delay(1000);
-    // }
+    for (int i = 0; i < 5; i++) {
+        digitalWrite(LED_BUILTIN, HIGH);
+        digitalWrite(PIN_LED_LARGE, HIGH);
+        delay(1000);
+        digitalWrite(LED_BUILTIN, LOW);
+        digitalWrite(PIN_LED_LARGE, LOW);
+        delay(1000);
+    }
 }
 
 void loop()
 {
-    // if (state.mode == Mode::INIT) {
-    //     // replace with user inputs values here
-    //     // state.tankCapacityGallons = TANK_CAPACITY_GALLONS;
-    //     // state.GPHTotal = GPH_TOTAL;
-    //     // state.waterTime = minutesToMillis(WATER_MINUTES);
-    //     if (state.tankCapacityGallons > 0 && state.GPHTotal > 0 && state.waterTime > 0) {
-    //         setState(state, Action::INIT_INTERVAL);
-    //     }
-    //     Serial.println("Mode: INIT");
-    //     tft.drawString("Mode: INIT", 20, 20, 2);
-    // }
-    // else
-    // {
-    //     if (state.mode == Mode::INTERVAL)
-    //     {
-    //         intervalMode(state);
-    //     }
-    //     else if (state.mode == Mode::MONITOR)
-    //     {
-    //         monitorMode(state);
-    //     }
-    //     button(state);
-    //     pump(state);
-    // }
+    if (state.mode == Mode::INIT) {
+        // replace with user inputs values here
+        // state.tankCapacityGallons = TANK_CAPACITY_GALLONS;
+        // state.GPHTotal = GPH_TOTAL;
+        // state.waterTime = minutesToMillis(WATER_MINUTES);
+        if (state.tankCapacityGallons > 0 && state.GPHTotal > 0 && state.waterTime > 0) {
+            setState(state, Action::INIT_INTERVAL);
+        }
+        tft.drawString("Mode: INTERVAL", 20, 20, 2);
+    }
+    else
+    {
+        if (state.mode == Mode::INTERVAL)
+        {
+            intervalMode(state);
+        }
+        else if (state.mode == Mode::MONITOR)
+        {
+            monitorMode(state);
+        }
+        button(state);
+        pump(state);
+    }
 }
