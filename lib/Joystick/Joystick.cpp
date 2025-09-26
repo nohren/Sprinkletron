5fad6d9ae4a1124a32c536946e71771dec6d970e
nohren <kuritake@protonmail.com>
2025-09-26 14:16:45 -0700

    added joystick library

---
+++ lib/Joystick/Joystick.cpp
@@ -0,0 +1,8 @@
+// void init() {
+//     pinMode(PIN_BUTTON_RIGHT, INPUT_PULLUP);
+//     pinMode(PIN_BUTTON_LEFT, INPUT_PULLUP);
+//     pinMode(PIN_JOY_SW, INPUT_PULLUP);
+//     analogReadResolution(12);
+//     analogSetPinAttenuation(PIN_JOY_X, ADC_11db);
+//     analogSetPinAttenuation(PIN_JOY_Y, ADC_11db);
+// }
